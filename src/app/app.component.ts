acd8d4741143376a57b2d2c23fd8d6f420650c70
rekqN <pmreisf@gmail.com>
2023-07-05 09:44:21 +0100

    pequenas correções

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'Sobre Mim', url: 'sobre-mim', icon: 'person' },
+    { title: 'Clube Desportivo', url: 'clube-desportivo', icon: 'football' },
+    { title: 'Gastronomia', url: 'gastronomia', icon: 'pizza' },
+    { title: 'Turismo', url: 'turismo', icon: 'location' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
